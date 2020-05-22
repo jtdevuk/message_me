135d2b136e63129e7f03b086079259269a429a7b
John Tweeddale <foureggs@hotmail.com>
2020-05-22 16:29:08 +0100

    Add user model and migration, bcrypt gem, hirb gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "John", password: "123")
+User.create(username: "Bob", password: "123")
+User.create(username: "Miska", password: "123")
+User.create(username: "Cam", password: "123")
+User.create(username: "Wass", password: "123")
