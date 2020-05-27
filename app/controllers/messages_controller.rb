5bb16f308253f925d240b79432e893bc528c9b1b
John Tweeddale <foureggs@hotmail.com>
2020-05-27 15:58:22 +0100

    update chatroom coffee to append styled message to chat box through messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,8 @@ before_action :require_user
   def create
     message = current_user.messages.build(message_params)
     if message.save
-      redirect_to root_path
+      ActionCable.server.broadcast "chatroom_channel",
+                                    mod_message: message_render(message)
     end
   end
 
@@ -13,6 +14,10 @@ private
 
 def message_params
   params.require(:message).permit(:body)
+end
+
+def message_render(message)
+  render(partial: 'message', locals: {message: message})
 end
 
 end
